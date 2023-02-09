61ee1ccef3b0f36ae3a636532880c7937c401592
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2023-02-09 13:56:11 -0300

    HolidayApi

---
+++ src/Data/HolidayData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Corviz\BrasilAPI\Data;
+
+use DateTimeImmutable;
+
+class HolidayData extends BaseData
+{
+    /**
+     * @param DateTimeImmutable $date
+     * @param string $name
+     * @param string $type
+     */
+    public function __construct(
+        public readonly DateTimeImmutable $date,
+        public readonly string $name,
+        public readonly string $type,
+    ) {
+    }
+}
---
+++ src/HolidayApi.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Corviz\BrasilAPI;
+
+use Corviz\BrasilAPI\Data\HolidayData;
+use DateTimeImmutable;
+use GuzzleHttp\Exception\GuzzleException;
+use ReflectionException;
+
+class HolidayApi extends ApiConsumer
+{
+    /**
+     * @param int|null $year (null for current year)
+     *
+     * @return HolidayData[]
+     * @throws GuzzleException
+     * @throws ReflectionException
+     */
+    public static function getByYear(?int $year = null): array
+    {
+        if (is_null($year)) {
+            $year = date('Y');
+        }
+
+        $response = static::getClient()->get("feriados/v1/$year");
+        $data = [];
+
+        if ($responseData = self::parseJson($response)) {
+            foreach ($responseData as $holiday) {
+                $holiday['date'] = DateTimeImmutable::createFromFormat('Y-m-d P', $holiday['date'].' -03:00');
+                $data[] = HolidayData::from($holiday);
+            }
+        }
+
+        return $data;
+    }
+}

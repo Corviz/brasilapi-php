207420ae3cfe2e329070b1b3ac5126bc72cbc260
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2023-02-10 10:00:46 -0300

    Keep field names in original language

--- src/Data/TaxData.php
+++ src/Data/TaxData.php
@@ -5,12 +5,12 @@ namespace Corviz\BrasilAPI\Data;
 class TaxData extends BaseData
 {
     /**
-     * @param string $name
-     * @param float $value
+     * @param string $nome
+     * @param float $valor
      */
     public function __construct(
-        public readonly string $name,
-        public readonly float $value,
+        public readonly string $nome,
+        public readonly float $valor,
     ) {
     }
 }
--- src/TaxApi.php
+++ src/TaxApi.php
@@ -4,12 +4,14 @@ namespace Corviz\BrasilAPI;
 
 use Corviz\BrasilAPI\Data\TaxData;
 use GuzzleHttp\Exception\GuzzleException;
+use ReflectionException;
 
 class TaxApi extends ApiConsumer
 {
     /**
-     * @return array
+     * @return TaxData[]
      * @throws GuzzleException
+     * @throws ReflectionException
      */
     public static function all(): array
     {
@@ -18,7 +20,7 @@ class TaxApi extends ApiConsumer
 
         if ($responseData = self::parseJson($response)) {
             foreach ($responseData as $tax) {
-                $data[] = self::createTaxData($tax);
+                $data[] = TaxData::from($tax);
             }
         }
 
@@ -26,31 +28,20 @@ class TaxApi extends ApiConsumer
     }
 
     /**
-     * @param string $name
-     * @return array|TaxData
+     * @param string $nome
+     * @return TaxData|null
      * @throws GuzzleException
+     * @throws ReflectionException
      */
-    public static function getByName(string $name)
+    public static function getByNome(string $nome): ?TaxData
     {
-        $response = static::getClient()->get("taxas/v1/$name");
-        $data = [];
+        $response = static::getClient()->get("taxas/v1/$nome");
+        $data = null;
 
         if ($responseData = self::parseJson($response)) {
-            $data = self::createTaxData($responseData);
+            $data = TaxData::from($responseData);
         }
 
         return $data;
-    }
-
-    /**
-     * @param array $apiData
-     * @return TaxData
-     */
-    private static function createTaxData(array $apiData): TaxData
-    {
-        return new TaxData(
-            name: $apiData['nome'],
-            value: $apiData['valor']
-        );
     }
 }

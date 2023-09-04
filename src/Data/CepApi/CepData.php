093132c8f7c2c93193aa6d1cc9ab99fad3862f2d
Carlos Alberto Bertholdo Carucce <carloscarucce@gmail.com>
2023-09-04 15:16:49 -0300

    CEP Data: makes neighbourhood and street attributes optional

--- src/Data/CepApi/CepData.php
+++ src/Data/CepApi/CepData.php
@@ -10,18 +10,18 @@ class CepData extends DataTransfer
      * @param string $cep
      * @param string $state
      * @param string $city
-     * @param string $neighborhood
-     * @param string $street
      * @param string $service
+     * @param ?string $street
+     * @param ?string $neighborhood
      * @param ?LocationData $location
      */
     public function __construct(
         public readonly string $cep,
         public readonly string $state,
         public readonly string $city,
-        public readonly string $neighborhood,
-        public readonly string $street,
         public readonly string $service,
+        public readonly ?string $street = null,
+        public readonly ?string $neighborhood = null,
         public readonly ?LocationData $location = null,
     ) {
     }
